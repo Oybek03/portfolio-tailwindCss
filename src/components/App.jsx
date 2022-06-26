c5da0aaa2a8b92d861cdf97af33ae97d10bdb386
Oybek <oybekjonxayrullayevich@gmail.com>
2022-06-26 18:51:32 +0500

    font link import

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-red-700">Hello</h1>
+    </div>
+  );
+};
+
+export default App;
