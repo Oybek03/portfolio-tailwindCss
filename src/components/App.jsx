5bdf424289e1866b3ab8d016e5c0da0ca770fd6f
Oybek <oybekjonxayrullayevich@gmail.com>
2022-06-27 10:01:41 +0500

    home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "../output.css";
 import Home from "./Home";
 import NavBar from "./NavBar";
+import SocialLinks from "./SocialLinks";
 const App = () => {
   return (
     <div>
       <NavBar />
       <Home />
+      <SocialLinks/> 
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-// import { heroImg } from "../images/photo_2022-06-26_22-19-23";
+import myImg from "../images/myImg.jpg";
 const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black to-gray-800"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 "
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div>
-          <h2>I'm Full Stack Developer</h2>
-          <p>
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl md:text-7xl font-bold text-white">
+            I'm Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
             aperiam, ipsam tempore aliquam voluptatum doloremque earum
             reiciendis repudiandae optio?
           </p>
           <div>
-            <button>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
               Portfolio
-              <span>
-                <MdOutlineKeyboardArrowRight />
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </button>
           </div>
         </div>
         <div>
-          <img src="././images/photo_2022-06-26_22-19-23" alt="my image" />
+          <img
+            src={myImg}
+            alt="my image"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
         </div>
       </div>
     </div>
