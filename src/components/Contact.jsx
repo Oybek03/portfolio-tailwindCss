c38853b2926827617ab6fe7e47809193d45c8f1c
Oybek <oybekjonxayrullayevich@gmail.com>
2022-06-28 09:40:26 +0500

    Contact component continues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>
-    Contact
-  </div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Contact</p>
+          <p>Submit the form below to get in touch with me</p>
+        </div>
+        <div>
+          <form action="">
+            <input type="text" placeholder="Ismingizni kiriting.." className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
